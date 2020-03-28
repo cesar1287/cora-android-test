724da6595321f7846922f15b6b36849ba3d2e961
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2020-03-28 16:08:28 -0300

    business/repository adjusted

--- home/src/main/java/com/github/coraandroidtest/home/business/HomeBusiness.kt
+++ home/src/main/java/com/github/coraandroidtest/home/business/HomeBusiness.kt
@@ -3,10 +3,14 @@ package com.github.coraandroidtest.home.business
 import com.github.coraandroidtest.base.BaseBusiness
 import com.github.coraandroidtest.core.database.dao.ContactDao
 import com.github.coraandroidtest.core.database.entity.Contact
+import com.github.coraandroidtest.home.repository.HomeRepository
+import org.koin.core.inject
 
 class HomeBusiness : BaseBusiness() {
 
+    private val repository: HomeRepository by inject()
+
     suspend fun getAllContacts(contactDao: ContactDao): List<Contact> {
-        return contactDao.getAllContacts()
+        return repository.getAllContacts(contactDao)
     }
 }
--- home/src/main/java/com/github/coraandroidtest/home/repository/HomeRepository.kt
+++ home/src/main/java/com/github/coraandroidtest/home/repository/HomeRepository.kt
@@ -1,5 +1,12 @@
 package com.github.coraandroidtest.home.repository
 
+import com.github.coraandroidtest.core.database.dao.ContactDao
+import com.github.coraandroidtest.core.database.entity.Contact
+
 class HomeRepository {
+
+    suspend fun getAllContacts(contactDao: ContactDao): List<Contact> {
+        return contactDao.getAllContacts()
+    }
 
 }
