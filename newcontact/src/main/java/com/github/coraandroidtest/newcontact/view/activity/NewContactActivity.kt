7c03eda62b5a756bda3ee038a203795d5a4768a2
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2020-03-30 12:03:41 -0300

    new contact fragments and navigation created

---
+++ newcontact/src/main/java/com/github/coraandroidtest/newcontact/view/activity/NewContactActivity.kt
@@ -0,0 +1,13 @@
+package com.github.coraandroidtest.newcontact.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.coraandroidtest.newcontact.R
+
+class NewContactActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_contact)
+    }
+}
