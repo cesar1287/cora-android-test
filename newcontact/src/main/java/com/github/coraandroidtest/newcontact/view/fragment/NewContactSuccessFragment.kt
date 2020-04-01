1d3aa4871a15308c5123c7daade4eb6bb473f2aa
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2020-04-01 13:49:28 -0300

    added finish to success new contact

--- newcontact/src/main/java/com/github/coraandroidtest/newcontact/view/fragment/NewContactSuccessFragment.kt
+++ newcontact/src/main/java/com/github/coraandroidtest/newcontact/view/fragment/NewContactSuccessFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 
 import com.github.coraandroidtest.newcontact.R
+import kotlinx.android.synthetic.main.fragment_new_contact_success.*
 
 class NewContactSuccessFragment : Fragment() {
 
@@ -16,6 +18,18 @@ class NewContactSuccessFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_new_contact_success, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupObservables()
+    }
+
+    private fun setupObservables() {
+        acbNewContactSuccess.setOnClickListener {
+            activity?.finish()
+        }
     }
 
 }
